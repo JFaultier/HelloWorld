0534533474e58532cf4abdce77c5cd5a431a7ffd
JFaultier <102233010+JFaultier@users.noreply.github.com>
2022-03-23 14:22:44 +0100

    Create java.java

---
+++ java.java
@@ -0,0 +1,9 @@
+/* 
+This is a programm from the GitHub Repository JFaultier/HelloWorld. More: github.com/JFaultier/HelloWorld/
+Dies ist ein Programm aus dem GitHub Repository JFaultier/HelloWorld. Mehr: github.com/JFaultier/HelloWorld/
+*/
+class java{
+  public static void main(String[]args){
+      System.out.println("Hello World in Java!");
+  }
+}
