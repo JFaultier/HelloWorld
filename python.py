8da7d9e71a9c1c1890b421823de7ff145d63d8a7
JFaultier <102233010+JFaultier@users.noreply.github.com>
2022-03-23 14:27:09 +0100

    Create python.py

---
+++ python.py
@@ -0,0 +1,3 @@
+#This is a programm from the GitHub Repository JFaultier/HelloWorld. More: github.com/JFaultier/HelloWorld/
+#Dies ist ein Programm aus dem GitHub Repository JFaultier/HelloWorld. Mehr: github.com/JFaultier/HelloWorld/
+print ("Hello World")
