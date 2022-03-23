707777e0ab931a926b1c08ae3af9c7b15667da61
JFaultier <102233010+JFaultier@users.noreply.github.com>
2022-03-23 14:28:46 +0100

    Create c.c

---
+++ c.c
@@ -0,0 +1,10 @@
+/*
+This is a programm from the GitHub Repository JFaultier/HelloWorld. More: github.com/JFaultier/HelloWorld/
+Dies ist ein Programm aus dem GitHub Repository JFaultier/HelloWorld. Mehr: github.com/JFaultier/HelloWorld/
+*/
+#include<stdio.h>
+
+int main() {
+	printf("Hello World\n");
+	return 0;
+}
