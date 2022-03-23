7a84fc49796f161c20f25284f27c074673319f44
JFaultier <102233010+JFaultier@users.noreply.github.com>
2022-03-23 14:44:03 +0100

    Create kotlin.kt

---
+++ kotlin.kt
@@ -0,0 +1,7 @@
+/* 
+This is a programm from the GitHub Repository JFaultier/HelloWorld. More: github.com/JFaultier/HelloWorld/
+Dies ist ein Programm aus dem GitHub Repository JFaultier/HelloWorld. Mehr: github.com/JFaultier/HelloWorld/
+*/
+fun main(args : Array<String>) {
+    println("Hello, World!")
+}
